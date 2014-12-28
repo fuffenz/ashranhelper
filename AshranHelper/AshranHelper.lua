d2a00d4f29929f408f5aa382c2f833d39d61997e
fuffenz <fuffenz@fuffenz.com>
2014-12-28 22:28:24 +0100

    changes for non-english clients

--- AshranHelper/AshranHelper.lua
+++ AshranHelper/AshranHelper.lua
@@ -1,11 +1,18 @@
 --
 --  AshranHelper 
+--  
 --
 
 local myFrame=CreateFrame("Frame")
+-- need to localize keywords...
 local keywords={"inv","invite"}
 local updateTimer = 0
 local playCounter = 0
+local ashranAreaID = 978;
+-- simple way to localize names for non-English clients
+local i18nAshran = GetMapNameByID(ashranAreaID):lower() -- "ashran";
+local i18nBrokenBones = GetItemInfo(118043) -- "Broken Bones"
+local isInAshran = false;
 
 function eventHandler(self,e,...) 
 	local arg1 = ...
@@ -14,21 +21,38 @@ function eventHandler(self,e,...)
 		if AshranHelperInviteEnabled == nil then
 			AshranHelperInviteEnabled = false;
 		end
+		if AshranHelperInvites == nil or AshranHelperBones == nil then
+			AshranHelperInvites = 0
+			AshranHelperBones = 0
+		end
 	end
 
-	-- print auto-invite status when we enter Ashran
-	if (e == "ZONE_CHANGED_NEW_AREA" and GetRealZoneText() == "Ashran") then
-		if AshranHelperInviteEnabled then
-			print("Ashran auto-invite is active")
+	if (e == "ZONE_CHANGED_NEW_AREA") then
+		SetMapToCurrentZone()
+		areaid = GetCurrentMapAreaID()
+
+		if(areaid == ashranAreaID) then
+			isInAshran = true;
+			-- print auto-invite status when we enter Ashran
+			if AshranHelperInviteEnabled then
+				print("Ashran auto-invite is active and will invite when you are in a raid group")
+			else
+				print("Ashran auto-invite is not active (do /ashranhelper auto to activate)")
+			end
 		else
-			print("Ashran auto-invite is not active (do /ashranhelper auto to activate)")
+			if(isInAshran) then
+				if(AshranHelperInviteEnabled) then
+					print("You have left Ashran and auto-invite is no longer active")
+				end
+				isInAshran = false
+			end
 		end
 	end
 
 	if (AshranHelperInviteEnabled and e == "CHAT_MSG_CHANNEL" or e == "CHAT_MSG_WHISPER" or e == "CHAT_MSG_SAY") then
 		local message,sender,_,_,_,_,_,_,chName,_,_,id = ...
 		-- look for messages in General - Ashran, but only if in raid and we have free spots
-		if IsInRaid() and (GetNumGroupMembers() < 40) and chName:lower():find("ashran") then
+		if IsInRaid() and (GetNumGroupMembers() < 40) and chName:lower():find(i18nAshran) then
 			local firstWord = message:match("(%S+).*")
 			if(firstWord ~= nil) then
 				for _,t in ipairs(keywords) 
@@ -36,6 +60,7 @@ function eventHandler(self,e,...)
 					if (t == firstWord:lower()) then 	
 						-- invite will fail if we're not leader/assist, doesn't really matter...					
 						InviteUnit(sender) 
+						AshranHelperInvites = AshranHelperInvites + 1
 						break 
 					end 
 				end 
@@ -48,7 +73,8 @@ end
 --  Filter out the spammy Broken Bones loot messages
 --
 function lootFilter(self, event, msg) 
-	if (msg:find("Broken Bones")) then
+	if (msg ~= nil and msg:find(i18nBrokenBones)) then
+		AshranHelperBones = AshranHelperBones + 1
 		return true
 	end
 	return false
@@ -61,12 +87,12 @@ function uiUpdate(self, elapsed)
 	updateTimer = updateTimer + elapsed
 	if(updateTimer > 2) then
 		for i=1, MAX_WORLD_PVP_QUEUES do
-		   local queueStatus, zone = GetWorldPVPQueueStatus(i)   
-			-- are there localized zone names?  no idea...
-			if(zone == "Ashran") then
+		    local queueStatus, zone = GetWorldPVPQueueStatus(i)   
+			
+			if(zone ~= nil and zone:lower() == i18nAshran) then
 			   	if(queueStatus == "confirm") then
 			   		if(playCounter == 0) then 
-						PlaySoundFile("sound\\interface\\levelup2.ogg")
+						PlaySoundFile("sound\\interface\\levelup2.ogg", "Master")
 					end
 					playCounter = playCounter + 1
 				else
@@ -100,6 +126,7 @@ function slashCommand(arg)
 		inviteStatus()
 	else
 		inviteStatus()
+		print("So far, " .. AshranHelperBones .. " bone spam suppressed and " .. AshranHelperInvites .. " invites sent")
 		print "To enable or disable auto-invite, use:"
 		print "/ashranhelper auto"
 	end
